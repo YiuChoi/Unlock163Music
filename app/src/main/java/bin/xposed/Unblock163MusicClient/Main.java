755c15154431f6cbbcc6c32a3a18bc22dede7c76
caiyao <whucaiyao@gmail.com>
2016-01-31 23:06:23 +0800

    兼容3.1.3和3.1.4版本

--- app/src/main/java/bin/xposed/Unblock163MusicClient/Main.java
+++ app/src/main/java/bin/xposed/Unblock163MusicClient/Main.java
@@ -1,20 +1,45 @@
 package bin.xposed.Unblock163MusicClient;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class Main implements IXposedHookLoadPackage {
+    public static final String packageName = "com.netease.cloudmusic";
+    public static String HOOK_UTILS;
+    public static String HOOK_CONSTRUCTOR;
+    public static final String VERSION_3_2_1 = "3.2.1";
+    public static String VERSION;
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 
-        if (lpparam.packageName.equals("com.netease.cloudmusic")) {
-            Utility.Init(lpparam.classLoader);
+        if (lpparam.packageName.equals(packageName)) {
+            final Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+            final Context systemContext = (Context) callMethod(activityThread, "getSystemContext");
+            VERSION = systemContext.getPackageManager().getPackageInfo(lpparam.packageName, 0).versionName;
+            switch (VERSION) {
+                case VERSION_3_2_1:
+                    HOOK_UTILS = "com.netease.cloudmusic.utils.n";
+                    HOOK_CONSTRUCTOR="com.netease.cloudmusic.i.b";
+                    break;
+                default:
+                    HOOK_UTILS = "com.netease.cloudmusic.utils.u";
+                    HOOK_CONSTRUCTOR="com.netease.cloudmusic.i.f";
+                    break;
+            }
 
-            //findAndHookMethod("com.netease.cloudmusic.utils.u", lpparam.classLoader,
-            findAndHookMethod("com.netease.cloudmusic.utils.n", lpparam.classLoader,
+            Utility.Init(lpparam.classLoader);
+            findAndHookMethod(HOOK_UTILS, lpparam.classLoader,
                     "i", new XC_MethodHook() {
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Exception {
